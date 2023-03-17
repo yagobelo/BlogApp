f93e16fc9c312709fb43dc844e53082b017c643e
yagobelo <yago.belo00@gmail.com>
2023-03-17 16:18:56 -0300

    Add Function Delete Category

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,4 @@
 const express = require("express");
-//const { default: mongoose } = require("mongoose");
 const router = express.Router();
 const mongoose = require("mongoose");
 require("../models/Categoria");
@@ -93,6 +92,16 @@ router.post("/categorias/edit", (req,res) => {
       res.redirect("/admin/categorias")
     })
   }
+})
+
+router.post("/categorias/deletar", ( req, res ) => {
+  Categoria.deleteOne({_id: req.body.id}).then( () => {
+    req.flash("success_msg", "Categoria deletada com sucesso!")
+    res.redirect("/admin/categorias")
+  }).catch((err) => {
+    req.flash("error_msg", "Houve um erro ao deletar categoria!")
+    res.redirect("/admin/categorias")
+  })
 })
 
 
