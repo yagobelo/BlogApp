a2ad7d78c52a757f9c50d1d9bb266dc895aad5ca
yagobelo <yago.belo00@gmail.com>
2023-03-22 18:16:57 -0300

    Add Delete postagens

--- routes/admin.js
+++ routes/admin.js
@@ -194,4 +194,16 @@ router.post('/postagens/edit', (req, res) => {
 
 })
 
+router.post("/postagens/deletar", (req, res) => {
+  Postagem.deleteOne({_id: req.body.id}).then(() => {
+    req.flash("success_msg", "Postagem deletada com sucesso.")
+    res.redirect("/admin/postagens")
+  }).catch((err) => {
+    req.flash("error_msg", "Houve um erro ao deletar a postagem")
+    res.redirect("/admin/postagens")
+  })
+})
+
+
+
 module.exports = router
