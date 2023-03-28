8a3608216613b2946318436f6efe21489c8615d4
yagobelo <yago.belo00@gmail.com>
2023-03-28 12:37:00 -0300

    Add logout from usuarios

--- helper/eAdmin.js
+++ helper/eAdmin.js
@@ -1,6 +1,6 @@
 module.exports = {
-  eAdmin: function(req, res, next){
-    if(req.isAuthenticated() && req.user.eAdmin == 1){
+  eAdmin: (req, res, next) => {
+    if(req.isAuthenticated()){
       return next();
     }
     req.flash("error_msg", "Você precisa ser um Admin!")
--- routes/usuario.js
+++ routes/usuario.js
@@ -88,4 +88,12 @@ router.post("/login", (req, res, next) => {
   })(req, res, next)
 })
 
+router.get("/logout", (req,res,next)=>{
+  req.logOut((err)=>{
+    if(err){return next(err)}    
+  req.flash('success_msg', "Deslogado com sucesso!")
+  res.redirect("/")
+  })
+})
+
 module.exports = router
