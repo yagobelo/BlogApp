378b5a1a725e611117cfd5a3988ad04ba6c269ee
yagobelo <yago.belo00@gmail.com>
2023-03-28 17:00:05 -0300

    Last Modifications

--- helper/eAdmin.js
+++ helper/eAdmin.js
@@ -1,6 +1,6 @@
 module.exports = {
   eAdmin: (req, res, next) => {
-    if(req.isAuthenticated()){
+    if(req.isAuthenticated() && req.user.eAdmin == 1){
       return next();
     }
     req.flash("error_msg", "Você precisa ser um Admin!")
