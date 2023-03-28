8649a0c256b3958dcedb43e1b6660effbab05f59
yagobelo <yago.belo00@gmail.com>
2023-03-27 22:12:45 -0300

    Correction Erro from last commit

--- app.js
+++ app.js
@@ -31,7 +31,10 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg")
   res.locals.error_msg = req.flash("error_msg")
+  res.locals.error = req.flash("error")
   next();
+
+
 })
 // bodyparser
 app.use(express.urlencoded({ extended: true }));
